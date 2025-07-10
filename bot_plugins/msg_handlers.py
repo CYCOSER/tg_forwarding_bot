80307c6d219e397f7a5c0991391d0e573da00115
CYCOSER <saikonaka570@gmail.com>
2025-07-10 13:11:23 +0300

    v1.12 Bug fixes

--- bot_plugins/msg_handlers.py
+++ bot_plugins/msg_handlers.py
@@ -12,76 +12,87 @@ handled_groups = set()
 async def start(client, cmd):
     with open("admin_list.json", "r") as f:
         admin_data = json.load(f)
-        if cmd.from_user.id in [i["id"] for i in admin_data]:
-            await cmd.reply_text(text=start_text)
+        try:
+            if cmd.from_user is not None and cmd.from_user.id in [i["id"] for i in admin_data]:
+                await cmd.reply_text(text=start_text)
 
-        else:
-            await cmd.reply_text(text=no_permission_text)
+            else:
+                await cmd.reply_text(text=no_permission_text)
+
+        except:
+            print("Error in start function")
 
 
 @Client.on_message(filters.command("list") & filters.private)
 async def fwdList(client, cmd):
     with open("admin_list.json", "r", encoding="utf-8") as f:
         admin_data = json.load(f)
-        if cmd.from_user.id in [i["id"] for i in admin_data]:
-            with open("config.json", "r", encoding="utf-8") as f:
-                data = json.load(f)
-                if len(data) <= 1:
-                    await cmd.reply_text(text="Похоже, что список пуст 🧐")
-                else:
-                    await cmd.reply_text(text='\n'.join([i["CHAT_NAME"] for i in data[1:]]))
+        try:
+            if cmd.from_user is not None and cmd.from_user.id in [i["id"] for i in admin_data]:
+                with open("config.json", "r", encoding="utf-8") as f:
+                    data = json.load(f)
+                    if len(data) <= 1:
+                        await cmd.reply_text(text="Похоже, что список пуст 🧐")
+                    else:
+                        await cmd.reply_text(text='\n'.join([i["CHAT_NAME"] for i in data[1:]]))
 
-        else:
-            await cmd.reply_text(text=no_permission_text)
+            else:
+                await cmd.reply_text(text=no_permission_text)
+
+        except:
+            print("Error in fwdlist function")
 
 
 @Client.on_message(filters.forwarded)
 async def forwarded_handler(client, message):
     with open("admin_list.json", "r", encoding="utf-8") as f:
         admin_data = json.load(f)
-        if message.from_user.id in [i["id"] for i in admin_data]:
-            group_id = message.media_group_id
+        try:
+            if message.from_user is not None and message.from_user.id in [i["id"] for i in admin_data]:
+                group_id = message.media_group_id
 
-            if group_id:
-                if group_id in handled_groups:
-                    return
+                if group_id:
+                    if group_id in handled_groups:
+                        return
 
-                handled_groups.add(group_id)
-                asyncio.create_task(remove_from_cache(group_id))
+                    handled_groups.add(group_id)
+                    asyncio.create_task(remove_from_cache(group_id))
 
-            if message.forward_from_chat:
-                channel_id = message.forward_from_chat.id
-                channel_name = message.forward_from_chat.title
-                print(channel_name)
-                await message.reply_text(
-                    text="📑 Вы хотели бы добавить этот канал в список, или удалить?",
-                    disable_web_page_preview=True,
-                    reply_markup=InlineKeyboardMarkup(
-                        [
+                if message.forward_from_chat:
+                    channel_id = message.forward_from_chat.id
+                    channel_name = message.forward_from_chat.title
+                    await message.reply_text(
+                        text="📑 Вы хотели бы добавить этот канал в список, или удалить?",
+                        disable_web_page_preview=True,
+                        reply_markup=InlineKeyboardMarkup(
                             [
-                                InlineKeyboardButton(
-                                    text="➕ Добавить канал",
-                                    callback_data=clipSpace64(f"acf{channel_id}{channel_name}"),
-                                ),
-                                InlineKeyboardButton(
-                                    text="➖ Удалить канал",
-                                    callback_data=clipSpace64(f"dcf{channel_id}{channel_name}"),
-                                )
-                            ],
-                            [
-                                InlineKeyboardButton(
-                                    text="❌ Отмена",
-                                    callback_data="back"
-                                )
+                                [
+                                    InlineKeyboardButton(
+                                        text="➕ Добавить канал",
+                                        callback_data=clipSpace64(f"acf{channel_id}{channel_name}"),
+                                    ),
+                                    InlineKeyboardButton(
+                                        text="➖ Удалить канал",
+                                        callback_data=clipSpace64(f"dcf{channel_id}{channel_name}"),
+                                    )
+                                ],
+                                [
+                                    InlineKeyboardButton(
+                                        text="❌ Отмена",
+                                        callback_data="back"
+                                    )
+                                ]
                             ]
-                        ]
+                        )
                     )
-                )
-            else:
-                await message.reply_text("⚠️ Похоже, что этот канал закрыт для ботов ⚠️")
+                else:
+                    await message.reply_text("⚠️ Похоже, что этот канал закрыт для ботов ⚠️")
 
-        else:
-            await message.reply_text(text=no_permission_text)
+            else:
+                await message.reply_text(text=no_permission_text)
+
+        except:
+            print("Error in forwarded_handler function")
 
 async def remove_from_cache(group_id):
     await asyncio.sleep(30)
